936b5b199d82f2a846ccb06a8fd80144dd7db9c7
pgurav-rgng <pradeep.gurav@gmail.com>
2025-04-25 16:21:32 +0530

    Modified NFT contract

--- ipfs/upload.js
+++ ipfs/upload.js
@@ -1,36 +1,43 @@
-async function run() {
-    const { create } = await import('ipfs-http-client');
-    const ipfs = await create();
-    
-    // we added three attributes, add as many as you want!
-    const metadata = {
-        path: '/',
-        content: JSON.stringify({
-            name: "My First NFT",
-            attributes: [
-            {
-                "trait_type": "Peace",
-                "value": "10" 
-            },
-            {
-                "trait_type": "Love",
-                "value": "100"
-            },
-            {
-                "trait_type": "Web3",
-                "value": "1000"
-            }
-            ],
-            // update the IPFS CID to be your image CID
-            image: "https://ipfs.io/ipfs/QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p",
-            description: "So much PLW3!"
-        })
-    };
+import { create } from "ipfs-http-client";
 
+const ipfs = create();
+// {
+//   url: "http://127.0.0.1:5001", // IPFS API endpoint
+// });
+
+async function run() {
+  const metadata = {
+    path: "/",
+    content: JSON.stringify({
+      name: "My First NFT",
+      attributes: [
+        {
+          trait_type: "Peace",
+          value: "10",
+        },
+        {
+          trait_type: "Love",
+          value: "100",
+        },
+        {
+          trait_type: "Web3",
+          value: "1000",
+        },
+      ],
+      image:
+        "https://ipfs.io/ipfs/QmXsS1x4DUaG7ELMMJHvWtzcwLu2kLvE1m2a72xpfFn9PN",
+      description: "So much PLW3!",
+    }),
+  };
+
+  try {
     const result = await ipfs.add(metadata);
     console.log(result);
+  } catch (err) {
+    console.error("Error adding metadata:", err);
+  }
 
-    process.exit(0);
+  process.exit(0);
 }
 
 run();
